4beb94014e0328c2d13f0c124c8ed505010e2659
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-14 10:16:41 +0100

    Add a task

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,23 @@ const App = () => {
     { id: nanoid(8), content: 'Faire du React' },
     { id: nanoid(8), content: 'Apprendre NodeJS' }
   ]);
-  // console.log(todoList);
+  
+  const [todo, setTodo] = useState('');
+
+  const [showValidation, setShowValidation] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo === "") {
+      setShowValidation(true);
+      return;
+    }
+    setTodoList([...todoList, { id: nanoid(8), content: todo }]);
+    setTodo('');
+    setShowValidation(false); 
+    return;
+  }
 
   const deleteTodo = (id) => {
     return setTodoList(todoList.filter((task) => task.id !== id));
@@ -20,15 +36,22 @@ const App = () => {
           To-do Liste
         </h1>
 
-        <form className="mb-10">
+        <form className="mb-10" onSubmit={handleSubmit}>
           <label htmlFor="todo-item" className='text-slate-50'>
             Ajouter une chose à faire
           </label>
-          <input type="text" className='mt-1 block w-full rounded' />
+          <input type="text" className='mt-1 block w-full rounded' value={todo} onChange={(e) => setTodo(e.target.value)} />
+          {showValidation && (
+            <p className="text-red-400">Ajoutez un contenu à pour créer une nouvelle tâche</p>
+          )}
           <button className='mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]'>Ajouter</button>
         </form>
         <ul>
-          {todoList.map(task => (
+          {todoList.length === 0 && (
+            <li className='text-slate-50 text-md'>Pas de tâche à faire ...</li>
+          )}
+          {todoList.length > 0 &&
+          todoList.map(task => (
             <ListItem key={task.id} content={task.content} deleteTask={() => deleteTodo(task.id)} />
           ))}
         </ul>
